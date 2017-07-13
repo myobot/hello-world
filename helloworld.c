a62b671aa7c4d8c7596c08577baabc0d6a9f887c
sherlock <18233225109@163.com>
2017-07-13 16:55:32 +0800

    stash commit

--- helloworld.c
+++ helloworld.c
@@ -4,5 +4,7 @@ int main()
     printf("hello-world\n");
     printf("creating a new branch is quick");
     printf("the new deff");
+    printf("git stash");
+    printf("git stash2");
     return 0;
 }
